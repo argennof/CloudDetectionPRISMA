7941cce12967873dcf256323405f22d32af82e35
argennof <d.fonnegra.m@gmail.com>
2025-12-09 12:21:14 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .src import preprocessing
+from .src import knn_classifier
+from .src import rf_classifier
+from .src import xgboost_classifier
